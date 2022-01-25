84475883e2d5a46fcf0390347ed873fa439c7b1a
Franco Palma <fpalmalopez2@gmail.com>
2022-01-25 15:42:59 -0300

    Original

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ const express      = require('express')
 const dotenv       = require('dotenv')
 const path         = require('path')
 const cookieParser = require('cookie-parser')
+const helmet       = require('helmet')
+const cors       = require('cors')
 
 const app = express()
 dotenv.config({ path: './.env'})
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 app.use(cookieParser())
+app.use(helmet())
+app.use(cors())
+
 
 app.set('port', process.env.PORT || 5000)
 app.use(express.static(path.join(__dirname, './src/public')));
